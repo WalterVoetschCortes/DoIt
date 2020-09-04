f5b9d72824fc4807d4f6d8192e0ff26a4088ea9c
WalterVoetschCortes <walter.vxd@googlemail.com>
2020-09-04 14:44:15 +0200

    communication between components

---
+++ src/app/_template/template-todo/template-todo.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { ToDo } from '../../_interface/todo';
+import {EventPing} from "../../_interface/eventping";
+
+@Component({
+  selector: 'app-template-todo',
+  templateUrl: './template-todo.component.html',
+  styleUrls: ['./template-todo.component.sass']
+})
+export class TemplateTodoComponent implements OnInit {
+
+  @Input() toDo$: ToDo;
+  @Output() ping: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public changeCheck(event?: any): void{
+    this.toDo$.status = !this.toDo$.status;
+    const eventObject: EventPing = {
+      label: 'check',
+      object: this.toDo$
+    }
+    this.ping.emit(eventObject);
+  }
+
+  public changeLabel(event?: any): void{
+    const eventObject: EventPing = {
+      label: 'label',
+      object: this.toDo$
+    }
+    this.ping.emit(eventObject);
+  }
+
+  public deleteToDo(event?: any): void{
+    const eventObject: EventPing = {
+      label: 'delete',
+      object: this.toDo$
+    }
+    this.ping.emit(eventObject);
+  }
+
+}
